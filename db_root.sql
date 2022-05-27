3637d7e0800043883abe1ff87315c423a3ec9ce7
azkafarghani <azkafarghani@apps.ipb.ac.id>
2022-05-27 09:50:35 +0700

    Project RPL

---
+++ db_root.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Bulan Mei 2022 pada 03.53
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_root`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_bacaan`
+--
+
+CREATE TABLE `tbl_bacaan` (
+  `id_user` varchar(15) NOT NULL,
+  `kategori` varchar(15) NOT NULL,
+  `bacaan` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(15) NOT NULL,
+  `nama_user` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `username` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `password` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`, `level`) VALUES
+(1, 'Azka ', 'admin', 'admin', 'admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
