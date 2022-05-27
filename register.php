9f694fa7fb3ab535876ec1c57390628f9a574506
azkafarghani <azkafarghani@apps.ipb.ac.id>
2022-05-27 14:18:00 +0700

    Revisi

---
+++ register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Regist</title>
+</head>
+
+<body>
+
+<form action="" method="post">
+    <ul>
+        <li>
+            <label for="username">username: </label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+        <label for="password">password: </label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+        <label for="password2">Konfirmasi password: </label>
+            <input type="password" name="password2" id="password">
+        </li>
+        <li>
+            <button type="submit" name="register">Register: 
+            </button>
+        </li>
+    </ul>
+
+</form>
+</body>
+
+</html>
