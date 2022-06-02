86f50e63e50b95088db4c79aee2e32fad5d7914f
azkafarghani <azkafarghani@apps.ipb.ac.id>
2022-06-03 04:35:23 +0700

    TERAKHIRRRR

--- bacaan_full.php
+++ bacaan_full.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Judul buku</title>
+    <link rel="stylesheet" href="TA/openbook.css">
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/be9c77a153.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Ini halaman full bacaan</h1>
-
     <?php 
         require 'koneksi.php';
         $id_bacaan = $_GET['id_bacaan'];
@@ -24,16 +25,31 @@
             MYSQLI_ASSOC
        );
 
-    //    print_r($konten_bacaan);
+       ?>
+    
 
     
 
-       echo "<img src=\"".$konten_bacaan[0]['gambar']."\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\"><br>";
-       echo $konten_bacaan[0]['judul_bacaan']."<br>";
-       echo $konten_bacaan[0]['nama_user']."<br>";
-       echo $konten_bacaan[0]['nama_kategori']."<br>";
-       echo $konten_bacaan[0]['konten']."<br>";
-
-    ?>
+   
+    
+    <div class="logo">
+        <img src="TA/rut.png" alt="">
+    </div>
+    <div class="all">
+    <div class="conten">
+    <?php echo "<img src=\"".$konten_bacaan[0]['gambar']."\" alt=\"gambar\" width=\"80\" height=\"140\"><br>"; ?>
+        <div class="tulisan"> 
+       <?php 
+       echo "Penulis: ".$konten_bacaan[0]['nama_user']."<br>";
+       echo "Genre: : ".$konten_bacaan[0]['nama_kategori']."<br>";
+       echo "<p style='font-weight:bold;'>" . $konten_bacaan[0]['judul_bacaan']."</p>";
+       echo $konten_bacaan[0]['konten']."<br>";   ?>
+        </div>
+    </div>
+       <div class="menu-bar">
+            <a href="" class="home"><i class="fa-solid fa-house "></i></a> <a href="" class="search"><i class="fa-solid fa-magnifying-glass"></i></a> <a href="" class="post"><i class="fa-solid fa-feather-pointed"></i></a> <a href="" class="fav"><i class="fa-solid fa-star"></i></a> <a href="" class="profile"><i class="fa-solid fa-user"></i></a>
+       </div>
+    </div>
+    
 </body>
 </html>
